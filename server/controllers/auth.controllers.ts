49523056c75962fc9df5e3586b71eb1eb65fba1d
Ikenna Iheanaetu <ikennadeveloper@gmail.com>
2025-05-11 12:50:05 +0100

    --Server-Setup

---
+++ server/controllers/auth.controllers.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from "express";
+
+const registerController = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+  } catch (error) {}
+};
+
+const loginController = (req: Request, res: Response, next: NextFunction) => {
+  try {
+  } catch (error) {}
+};
