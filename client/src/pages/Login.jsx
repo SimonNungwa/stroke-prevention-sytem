633d7decf63e64e91c5af2a96ee9b89a4eba5438
SimonNungwa <simonnungwa99@gmail.com>
2025-05-12 00:40:46 +0100

    Add login component

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+
+const  Login = () => {
+    return (
+        <>
+           <form method={'post'}>
+               
+           </form>
+        </>
+    )
+};
+
+export default Login;
