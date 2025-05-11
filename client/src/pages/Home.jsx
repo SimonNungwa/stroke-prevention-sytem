b5aa16337eee9fad807f617c9f9dd15267f74ed7
SimonNungwa <simonnungwa99@gmail.com>
2025-05-12 00:42:22 +0100

    Add Home component

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "../components/Nav";
+import MobileNav from "../components/MobileNav";
+
+const Home = () => {
+    return(
+        <>
+            <Nav />
+            <MobileNav />
+        </>
+    )
+};
+
+export default Home;
